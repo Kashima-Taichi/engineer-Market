c418f8c8bdb3d61ae67037c95a3553556351d85c
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-09-24 21:23:21 +0900

    スクレイピング対象の言語の追加と処理の関数化

--- levtech.py
+++ levtech.py
@@ -2,8 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 import datetime
 
+# TODO: CSVで1ファイル管理
 
 # ########## variables ##########
+
+# Datetime関係
+dt = datetime.datetime.now()
+formatted = dt.strftime('%Y%m')
 
 # レバテックサイトトップのURL
 siteUrl = 'https://freelance.levtech.jp/'
@@ -21,8 +26,17 @@ phpJobsUrl = detailJobsBaseURL + 'skill-5/'
 # Javaの求人ページURL
 javaJobsUrl = detailJobsBaseURL + 'skill-3/'
 
+# pythonの求人ページURL
+pyJobsUrl = detailJobsBaseURL + 'skill-7/'
+
 # Rubyの求人ページURL
 rubyJobsUrl = detailJobsBaseURL + 'skill-8/'
+
+# Golangの求人ページURL
+golangJobsUrl = detailJobsBaseURL + 'skill-10/'
+
+# javascriptの求人ページURL
+javascriptJobsUrl = detailJobsBaseURL + 'skill-4/'
 
 # PHP求人ページの求人カード内のwebエレメント
 jobTitleAnchorClass = 'js-link_rel'
@@ -52,6 +66,67 @@ last = 150
 # ########## variables ##########
 
 
+# ########## function ##########
+
+
+def scrape_job_detail(sourceUrl, languageName):
+    Page = requests.get(sourceUrl)  # ←のURLを関数の引数に設定する
+
+    soup = BeautifulSoup(Page.text, 'lxml')
+
+    jobTitles = soup.find_all(anchorTag, {classAttribute: jobTitleAnchorClass})
+    jobRewards = soup.find_all(listTag, {classAttribute: jobRewardListClass})
+    jobSkills = soup.select(jobSkillParagraphSelector)
+
+    fileName = formatted + languageName + ".txt"  # ←のphpのベタがきを関数の引数へ
+
+    f = open(fileName, 'w')
+    for i in range(loops):
+        f.write(str(jobTitles[i].text) + ',')
+        reward = int(jobRewards[i].find(
+            spanTag).text.replace(',', '').replace('円', ''))
+        if reward < hourlyWageFlag:
+            reward = reward * monthlyHours
+        f.write(str(reward) + ',')
+        f.write(str(jobSkills[i].contents) + '\n')
+
+    # 求人ページで単価を取得 (2ページ目以降)
+    try:
+        for i in range(second, last):
+            Page = requests.get(
+                sourceUrl + 'p' + str(i) + '/')  # ←のURLを関数の引数に設定する
+            soup = BeautifulSoup(Page.text, 'lxml')
+            jobTitles = soup.find_all(
+                anchorTag, {classAttribute: jobTitleAnchorClass})
+            jobRewards = soup.find_all(
+                listTag, {classAttribute: jobRewardListClass})
+            jobSkills = soup.select(jobSkillParagraphSelector)
+
+            for j in range(loops):
+                f.write(str(jobTitles[j].text) + ',')
+                reward = int(jobRewards[j].find(
+                    spanTag).text.replace(',', '').replace('円', ''))
+                if reward < hourlyWageFlag:
+                    reward = reward * monthlyHours
+                f.write(str(reward) + ',')
+                f.write(str(jobSkills[j].contents) + '\n')
+
+            print(i)
+
+    except Exception as e:
+        print(languageName + 'の求人は' + str(i) + 'ページで処理が終了')
+        print(e)
+
+    f.close()
+
+
+# ########## function ##########
+
+
+# #####################################
+# サイトトップにおける言語別の求人数の取得
+# #####################################
+
 # レバテックフリーランスにアクセスしてコンテンツの取得
 html = requests.get(siteUrl)
 soup = BeautifulSoup(html.text, 'lxml')
@@ -61,9 +136,6 @@ langJobs = soup.find(divTag, {classAttribute: languageJobsBox})
 jobs = langJobs.find_all(listTag, {classAttribute: languageJobsList})
 
 # テキストファイルへ言語別の求人情報を書き込み
-# TODO: CSVで1ファイル管理
-dt = datetime.datetime.now()
-formatted = dt.strftime('%Y%m')
 fileName = formatted + "jobs.txt"
 
 with open(fileName, 'w') as f:
@@ -72,163 +144,25 @@ with open(fileName, 'w') as f:
         print(str(job.find(spanTag).contents), file=f)
 
 
-# #####################################
-# PHPの求人ページで単価を取得 (1ページ目)
-# #####################################
-
-phpPage = requests.get(phpJobsUrl)
-soup = BeautifulSoup(phpPage.text, 'lxml')
-
-jobTitles = soup.find_all(anchorTag, {classAttribute: jobTitleAnchorClass})
-jobRewards = soup.find_all(listTag, {classAttribute: jobRewardListClass})
-jobSkills = soup.select(jobSkillParagraphSelector)
-
-fileName = formatted + "php.txt"
+# PHPの求人ページで単価を取得
+scrape_job_detail(phpJobsUrl, 'php')
 
 
-f = open(fileName, 'w')
-for i in range(loops):
-    f.write(str(jobTitles[i].text) + ',')
-    reward = int(jobRewards[i].find(
-        spanTag).text.replace(',', '').replace('円', ''))
-    if reward < hourlyWageFlag:
-        reward = reward * monthlyHours
-    f.write(str(reward) + ',')
-    f.write(str(jobSkills[i].contents) + '\n')
-
-# PHPの求人ページで単価を取得 (2ページ目以降)
-try:
-    for i in range(second, last):
-        phpPage = requests.get(
-            phpJobsUrl + 'p' + str(i) + '/')
-        soup = BeautifulSoup(phpPage.text, 'lxml')
-        jobTitles = soup.find_all(
-            anchorTag, {classAttribute: jobTitleAnchorClass})
-        jobRewards = soup.find_all(
-            listTag, {classAttribute: jobRewardListClass})
-        jobSkills = soup.select(jobSkillParagraphSelector)
-
-        for j in range(loops):
-            f.write(str(jobTitles[j].text) + ',')
-            reward = int(jobRewards[j].find(
-                spanTag).text.replace(',', '').replace('円', ''))
-            if reward < hourlyWageFlag:
-                reward = reward * monthlyHours
-            f.write(str(reward) + ',')
-            f.write(str(jobSkills[j].contents) + '\n')
-
-        print(i)
-
-except Exception as e:
-    print(str(i) + 'ページで処理が終了')
-    print(e)
-
-f.close()
+# Rubyの求人ページで単価を取得
+scrape_job_detail(rubyJobsUrl, 'ruby')
 
 
-# #####################################
-# Rubyの求人ページで単価を取得 (1ページ目)
-# #####################################
-
-rubyPage = requests.get(rubyJobsUrl)
-soup = BeautifulSoup(rubyPage.text, 'lxml')
-
-jobTitles = soup.find_all(anchorTag, {classAttribute: jobTitleAnchorClass})
-jobRewards = soup.find_all(listTag, {classAttribute: jobRewardListClass})
-jobSkills = soup.select(jobSkillParagraphSelector)
-
-fileName = formatted + "ruby.txt"
+# Javaの求人ページで単価を取得
+scrape_job_detail(javaJobsUrl, 'java')
 
 
-f = open(fileName, 'w')
-for i in range(loops):
-    f.write(str(jobTitles[i].text) + ',')
-    reward = int(jobRewards[i].find(
-        spanTag).text.replace(',', '').replace('円', ''))
-    if reward < hourlyWageFlag:
-        reward = reward * monthlyHours
-    f.write(str(reward) + ',')
-    f.write(str(jobSkills[i].contents) + '\n')
-
-# rubyの求人ページで単価を取得 (2ページ目以降)
-try:
-    for i in range(second, last):
-        rubyPage = requests.get(
-            rubyJobsUrl + 'p' + str(i) + '/')
-        soup = BeautifulSoup(rubyPage.text, 'lxml')
-        jobTitles = soup.find_all(
-            anchorTag, {classAttribute: jobTitleAnchorClass})
-        jobRewards = soup.find_all(
-            listTag, {classAttribute: jobRewardListClass})
-        jobSkills = soup.select(jobSkillParagraphSelector)
-
-        for j in range(loops):
-            f.write(str(jobTitles[j].text) + ',')
-            reward = int(jobRewards[j].find(
-                spanTag).text.replace(',', '').replace('円', ''))
-            if reward < hourlyWageFlag:
-                reward = reward * monthlyHours
-            f.write(str(reward) + ',')
-            f.write(str(jobSkills[j].contents) + '\n')
-
-        print(i)
-
-except Exception as e:
-    print(str(i) + 'ページで処理が終了')
-    print(e)
-
-f.close()
+# pythonの求人ページで単価を取得
+scrape_job_detail(pyJobsUrl, 'python')
 
 
-# #####################################
-# Javaの求人ページで単価を取得 (1ページ目)
-# #####################################
-
-javaPage = requests.get(javaJobsUrl)
-soup = BeautifulSoup(javaPage.text, 'lxml')
-
-jobTitles = soup.find_all(anchorTag, {classAttribute: jobTitleAnchorClass})
-jobRewards = soup.find_all(listTag, {classAttribute: jobRewardListClass})
-jobSkills = soup.select(jobSkillParagraphSelector)
-
-fileName = formatted + "java.txt"
+# golangの求人ページで単価を取得
+scrape_job_detail(golangJobsUrl, 'golang')
 
 
-f = open(fileName, 'w')
-for i in range(loops):
-    f.write(str(jobTitles[i].text) + ',')
-    reward = int(jobRewards[i].find(
-        spanTag).text.replace(',', '').replace('円', ''))
-    if reward < hourlyWageFlag:
-        reward = reward * monthlyHours
-    f.write(str(reward) + ',')
-    f.write(str(jobSkills[i].contents) + '\n')
-
-# javaの求人ページで単価を取得 (2ページ目以降)
-try:
-    for i in range(second, last):
-        javaPage = requests.get(
-            javaJobsUrl + 'p' + str(i) + '/')
-        soup = BeautifulSoup(javaPage.text, 'lxml')
-        jobTitles = soup.find_all(
-            anchorTag, {classAttribute: jobTitleAnchorClass})
-        jobRewards = soup.find_all(
-            listTag, {classAttribute: jobRewardListClass})
-        jobSkills = soup.select(jobSkillParagraphSelector)
-
-        for j in range(loops):
-            f.write(str(jobTitles[j].text) + ',')
-            reward = int(jobRewards[j].find(
-                spanTag).text.replace(',', '').replace('円', ''))
-            if reward < hourlyWageFlag:
-                reward = reward * monthlyHours
-            f.write(str(reward) + ',')
-            f.write(str(jobSkills[j].contents) + '\n')
-
-        print(i)
-
-except Exception as e:
-    print(str(i) + 'ページで処理が終了')
-    print(e)
-
-f.close()
+# javascriptの求人ページで単価を取得
+scrape_job_detail(javascriptJobsUrl, 'javascript')
